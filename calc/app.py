586ec66876c4694e16b85b854cd1664530d28ffa
Shahabr99 <sean.rohani9@gmail.com>
2023-03-24 19:18:15 -0700

    Flask assignments are done

--- calc/app.py
+++ calc/app.py
@@ -1,5 +1,24 @@
 # Put your app in here.
-from flask import Flask
+from flask import Flask, request
+from operations import *
+
+
 
 app = Flask(__name__)
 
+@app.route('/<operation>')
+def calculate(operation):
+  num1 = int(request.args.get('a'))
+  num2 = int(request.args.get('b'))
+ 
+  if operation == 'add':
+    result = add(num1, num2)
+  elif operation == 'subtract':
+    result = sub(num1, num2)
+  elif operation == 'division':
+    result = div(num1, num2)
+  elif operation == 'multiply':
+    result = mult(num1, num2)
+  else:
+    return 'invalid operation'
+  return str(result)
